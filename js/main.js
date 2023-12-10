6450edb56e9353ff9f0ed7c4b3667a81c03e210f
Hernán Ezequiel Novara <her.novara@hotmail.com>
2023-12-10 19:04:02 -0300

    estilos-peliculas

--- js/main.js
+++ js/main.js
@@ -1,4 +1,3 @@
-
 const cssFiles = ["./style/estilos.css", "./style/estilos-retro.css", "./style/estilos-peliculas.css"]
 let activeFile = 0;
 const links = document.getElementsByTagName("link")
@@ -10,5 +9,3 @@ function onClick() {
     links[0].href = cssFiles[activeFile]
     console.log(links[0].href)
 }
-
-
